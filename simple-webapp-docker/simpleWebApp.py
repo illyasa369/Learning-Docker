bd0764a8fa21e67280a27b8606499f1f00d14532
IllyasWSL <illyasmado@outlook.com>
2025-12-24 15:26:16 +0000

    Containerise basic web app with Docker
    Updated simpleWebApp.py

---
+++ simple-webapp-docker/simpleWebApp.py
@@ -0,0 +1,12 @@
+from flask import Flask 
+
+
+webApp = Flask(__name__) 
+
+@webApp.route('/')
+def hello():
+    return 'Hello from a Docker container!'
+
+
+if  __name__ == '__main__':
+    webApp.run(host='0.0.0.0', port=5000)
